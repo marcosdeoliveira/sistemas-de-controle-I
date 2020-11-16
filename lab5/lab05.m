027ece5090b4d7da8b1e10be18e236e9577eb4dd
unknown <marcos.dee.oliveira@gmail.com>
2020-11-16 04:13:20 -0300

    initial commit

--- lab5/lab05.m
+++ lab5/lab05.m
@@ -10,25 +10,35 @@ t = 0:0.01:10; %tempo em analise
 %dados do enunciado:
 P = ( 400/(s^2 + 30*s + 200) ) %modelo da planta
 Eest = 0.1; %erro estacionário desejado
-R = 1/s^2
+R = 1/s; %sinal de entrada  
 
-%encontrouse um controlador proporcional
-%que garante o erro estacionario com valor desejado
-K = 5/s
-
-G = K*P
-
+## Encontrouse um controlador proporcional
+## que garante o erro estacionario com valor desejado K = 5/s
+K = 5/s;
+G = K*P;
 H=1;
-
 T = G/(1+G*H) %funcao de transferencia no ramo direto
 
-%E = R/(1+G)
+## sinal de resposta do sistema
+Y = step(T/s,t); 
+## sinal de referencia (rampa unitaria)
+Y2 = step(R,t);
 
-
-Y = step(T/s,t); %
-
-Y2 = step(1/s,t); %erro
-
+## calculo do erro
 E = Y2-Y
-figure,plot(t, Y, "-b", t, Y2, "-r")
-plot(t,E,"--g")
+
+## Comportamento do sistema
+figure,plot(t, Y, "-b", t, Y2, "--r")
+
+legend("resposta do sistema","refer\ência");
+title_h = title('Resposta a rampa unitária', 'fontsize', 15);
+xlabel('Tempo(s)', 'fontsize', 15);
+ylabel('Amplitude', 'fontsize', 15);
+
+## Comportamento do valor de erro
+figure,plot(t,E,"-b",[min(t), max(t)],[Eest Eest], '--r','linewidth',1);
+
+legend("erro estácion\ário");
+title_h = title('Resposta a rampa unitária', 'fontsize', 15);
+xlabel('Tempo(s)', 'fontsize', 15);
+ylabel('Amplitude', 'fontsize', 15);
